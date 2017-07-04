36189cdd96435bd6093cabf25e59dc530019c538
a-rutnam <a.rutnam@gmail.com>
2017-07-04 15:52:19 +1000

    airplane scaffold 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704054837) do
+ActiveRecord::Schema.define(version: 20170704055031) do
+
+  create_table "airplanes", force: :cascade do |t|
+    t.integer  "rows"
+    t.integer  "columns"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
