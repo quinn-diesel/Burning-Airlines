cbb3a15a4dbe7cb076ef1125c236399b0ad06544
Lingxiao Wang <Lingxiao@MacBook-Pro-2.local>
2017-07-05 16:06:53 +1000

    flight detail view working

--- app/assets/javascripts/models/Flight.js
+++ app/assets/javascripts/models/Flight.js
@@ -4,10 +4,7 @@ var app = app || {}
 app.Flight =  Backbone.Model.extend({
   urlRoot: '/flights',
 
-  defaults:{
-    from: 'Sydney',
-    to: 'Melbourne'
-  },
+
 
   initialize: function(){
     console.log("New Flight Ready");
