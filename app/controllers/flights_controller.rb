bc53578820dc04a4a7a3b43798eb6c76730a8e6d
a-rutnam <a.rutnam@gmail.com>
2017-07-05 17:22:14 +1000

    getting rid of weird head stuff so i can pull

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -11,10 +11,7 @@ class FlightsController < ApplicationController
   # GET /flights/1
   # GET /flights/1.json
   def show
-<<<<<<< HEAD
-=======
       render :json => @flight, :include => :airplane
->>>>>>> 53888b0c9ff0ea9f59f65a32b074acf886870c5a
 
   end
 
