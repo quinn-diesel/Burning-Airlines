32695cdbbb49f3aeb9add0b2becc3628ab86d847
Lingxiao Wang <Lingxiao@MacBook-Pro-2.local>
2017-07-06 12:34:14 +1000

    res

--- app/assets/javascripts/views/ShowReservationView.js
+++ app/assets/javascripts/views/ShowReservationView.js
@@ -1,11 +1,21 @@
 var app = app || {};
 
 app.ShowReservationView = Backbone.View.extend({
-  el: '#app',
+  tagName: 'tr',
 
   render: function (){
 
-  
+    console.log(this.collection);
+
+    _.each(this.collection.models, function( res ){
+      var rawTemplate = $('#ShowReservationTemplate').html();
+      var template = _.template( rawTemplate );
+      var markup = template(res.attributes);
+      this.$el.html( markup ).appendTo('#reservations');
+      console.log(res);
+    });
+
+    $('#reservations').show();
 
   }
 
